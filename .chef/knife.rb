b70edb09c92c9a0b604cf47dfa67aa2ffdec5b1c
tsuyeet <tendo.3201@gmailcom>
2014-01-28 00:21:09 +0900

    update knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,8 +1,9 @@
-cookbook_path    ["cookbooks", "site-cookbooks"]
+cookbook_path    ["./cookbooks", "site-cookbooks"]
 node_path        "nodes"
 role_path        "roles"
 environment_path "environments"
 data_bag_path    "data_bags"
+client_key '/Users/endo/.chef/tsuyeet.pem'
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
